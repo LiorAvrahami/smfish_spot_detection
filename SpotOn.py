22a40a5749f7d37bf7bcd01f019a783fc63e3e81
Lior_Avrahami <Lior.2pi@gmail.com>
2023-08-29 17:58:06 +0300

    .
    Signed-off-by: Lior_Avrahami <Lior.2pi@gmail.com>

--- SpotOn.py
+++ SpotOn.py
@@ -11,9 +11,13 @@ from image_manipulation.standardize_image import normalize_image
 
 # now comes the neural network
 from noise_reduction.model_denoise import DenoiseNet
+from neural_network.model_classifier import spots_classifier_net as ClassifierNet
 
 image_cleaner_network = DenoiseNet()
-image_cleaner_network.load_state_dict(torch.load('denoiseNet.pt'))
+image_cleaner_network.load_state_dict(torch.load('denoiseNet.pt', map_location=torch.device('cpu')))
+
+classifier_network = ClassifierNet()
+classifier_network.load_state_dict(torch.load("classifyNet.pt", map_location=torch.device('cpu')))
 
 
 def clean_image(image: npt.NDArray) -> npt.NDArray:
@@ -26,29 +30,48 @@ def clean_image(image: npt.NDArray) -> npt.NDArray:
     return pred.numpy()
 
 
-def classify_roi(small_image: npt.NDArray):
-    has_point = True  # bool(np.random.choice([False, True], p=[0.99, 0.01]))  # TODO feed im_temp to classifier_network
-    result_channel = 0
+def classify_roi_list_Moc(small_image: npt.NDArray):
+    has_point = np.full(len(small_image), True)
+    result_channel = np.zeros(has_point.shape)
     return has_point, result_channel
 
 
-def locate_spots(image: npt.NDArray, expected_point_density, false_neg_to_false_pos_ratio, b_use_denoising=True,) -> list[tuple[int, int, int, int]]:
+def classify_roi_list(small_images_list: npt.NDArray, cutoff=0.5):
+    images = torch.tensor(small_images_list)  # type: ignore
+    images = images.permute(0, 4, 3, 2, 1)  # get ch,z,x,y # type: ignore
+    pred = classifier_network(images.float()).detach()
+    has_point = pred.numpy() > cutoff
+    result_channel = np.zeros(has_point.shape)
+    return has_point, result_channel
+
+
+def locate_spots(image: npt.NDArray, expected_point_density, false_neg_to_false_pos_ratio, b_use_denoising=True, b_use_classifier=True) -> list[tuple[int, int, int, int]]:
     result_detected_points = []
 
     # clean image with noise reduction net
     if b_use_denoising:
         image = clean_image(image)
 
-    num_roi_per_channel = expected_point_density * image.size * 1000  # we want 1000 points per roi.
+    num_roi_per_channel = int(expected_point_density * image.size * 1000)  # we want 1000 points per roi.
     points_of_interest = list(zip(*np.where(find_points_of_interest(image, num_roi_per_channel))))  # type: ignore
     points_of_interest: list[tuple[int, int, int]]  # points_of_interest = list of (x,y,z) points
+    small_images_arr = []
     for point in points_of_interest:
         small_image = crop(image, point[0], point[1])
         if not is_valid_size(small_image):
             continue
-        has_point, result_channel = classify_roi(small_image)
-        if has_point:
-            result_detected_points.append((point[1], point[0], point[2], result_channel))
+        small_images_arr.append(small_image)
+    small_images_arr = np.array(small_images_arr)
+
+    if b_use_classifier:
+        has_point_list, result_channels_list = classify_roi_list(small_images_arr)
+    else:
+        has_point_list, result_channels_list = classify_roi_list_Moc(small_images_arr)
+
+    for point_index in range(len(has_point_list)):
+        if has_point_list[point_index]:
+            point = points_of_interest[point_index]
+            result_detected_points.append((point[1], point[0], point[2], result_channels_list[point_index]))
 
     return result_detected_points
 
@@ -63,5 +86,5 @@ def export_spots_to_csv_file(points, file_name):
 if __name__ == "__main__":
     image = convert_image_file_to_numpy("images\\tagged_images_validation\\img2\\image.tif")
     image = normalize_image(image)
-    detected_points = locate_spots(image, 0.0001, 1)
+    detected_points = locate_spots(image, 1000, 1)
     export_spots_to_csv_file(detected_points, f"Result_{np.random.randint(1000)}.csv")
--- regions_of_interest_module/regions_of_interest.py
+++ regions_of_interest_module/regions_of_interest.py
@@ -7,14 +7,15 @@ B_USE_GAUSSIAN_LAPLACE = False
 
 def find_points_of_interest(image, num_poi_per_channel):
     points_of_interest = np.full(image.shape[:-1], False)
-    for ch_index in [0]:  # range(image.shape[3]):
+    for ch_index in range(image.shape[3]):
         im = image[:, :, :, ch_index]
         if B_USE_GAUSSIAN_LAPLACE:
             im = scipy.ndimage.gaussian_laplace(im, sigma=1, mode="mirror") * (-1)
 
         is_local_maximum = im == scipy.ndimage.maximum_filter(im, size=3, mode="reflect")
         local_maximum_values = im[is_local_maximum]
-        cutoff_val = np.sort(local_maximum_values)[-num_poi_per_channel]
+        # find the cutoff value, this value is the value of the num_poi_per_channel'th brightest local maximum after cleaning.
+        cutoff_val = np.sort(local_maximum_values)[max(-num_poi_per_channel, 0)]
 
         indexes_points = im > cutoff_val
 
